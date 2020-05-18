ffecef6faa9705a6ea36012822a0919ea55bfa74
Leah Silber <LeahSilber@gmail.com>
2020-05-18 12:13:37 -0700

    Done.

--- index.js
+++ index.js
@@ -54,5 +54,145 @@ const fi = {
     return accumulator;
   },
 
-  functions() {},
+  // find(collection, predicate) {
+  //   let returnValue;
+  //   fi.each(collection, (value, key, collection) => {
+  //     if (predicate(value) === true) {
+  //       return (returnValue = value);
+  //     }
+  //   });
+  //   return returnValue;
+  // },
+
+  find(collection, predicate) {
+    for (let i = 0; i < collection.length; i++) {
+      if (predicate(collection[i]) === true) {
+        return collection[i];
+      }
+    }
+  },
+
+  filter(collection, predicate) {
+    let newArr = [];
+    fi.each(collection, (value, key, collection) => {
+      if (predicate(value) === true) {
+        newArr.push(value);
+      }
+      // return predicate(value) === true ? value : false;
+    });
+    return newArr;
+  },
+
+  size(collection) {
+    if (Symbol.iterator in collection) {
+      return collection.length;
+    } else {
+      return Object.entries(collection).length;
+    }
+  },
+
+  first(array, n) {
+    if (!n) {
+      return array[0];
+    } else {
+      return array.slice(0, n);
+    }
+  },
+
+  last(array, n) {
+    if (!n) {
+      let index = array.length - 1;
+      return array[index];
+    } else {
+      return array.slice(array.length - n, array.length);
+    }
+  },
+
+  compact(array) {
+    return fi.filter(array, (item, i, collection) => {
+      return !!item;
+    });
+  },
+
+  sortBy(array, callback) {
+    let newArr = [...array];
+
+    newArr.sort((a1, b1) => {
+      let a = callback(a1);
+      let b = callback(b1);
+
+      if (a < b) {
+        return -1;
+      } else if (a > b) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    return newArr;
+  },
+
+  flatten(array, shallow, newArr = []) {
+    if (!Array.isArray(array)) return newArr.push(array);
+
+    if (!shallow) {
+      array.forEach((item) => {
+        fi.flatten(item, false, newArr);
+      });
+    } else {
+      array.forEach((item) => {
+        if (Array.isArray(item)) {
+          extract(item, newArr);
+        } else {
+          newArr.push(item);
+        }
+      });
+    }
+    return newArr;
+  },
+
+  uniq(array, isSorted = false, callback) {
+    let newArr = [];
+    if (callback) {
+      let seen = [];
+      array.forEach((el) => {
+        let result = callback(el);
+
+        if (!seen.includes(result)) {
+          seen.push(result);
+          newArr.push(el);
+        }
+      });
+    } else {
+      array.forEach((el) => {
+        if (!newArr.includes(el)) {
+          newArr.push(el);
+        }
+      });
+    }
+
+    return newArr;
+  },
+
+  keys(object) {
+    return Object.getOwnPropertyNames(object).filter(
+      (name) => Object.getOwnPropertyDescriptor(object, name).enumerable
+    );
+  },
+
+  values(object) {
+    let keys = this.keys(object);
+    return keys.map((key) => object[key]);
+  },
+
+  functions(object) {
+    let keys = this.keys(object);
+    return keys.filter((key) => typeof object[key] === "function");
+  },
 };
+
+function extract(Arr, newArr) {
+  for (let item of Arr) {
+    newArr.push(item);
+  }
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -288,6 +288,7 @@ describe("index.js", function () {
     it("correctly flattens a ludicrously nested array", function () {
       const nestedArr = [1, [2, 3], [[4, 5], 6, [7, [8, 9]]]];
       const flatArr = fi.flatten(nestedArr);
+
       expect(arraysEqual(flatArr, [1, 2, 3, 4, 5, 6, 7, 8, 9])).to.equal(true);
     });
 
@@ -316,7 +317,7 @@ describe("index.js", function () {
 
     it("removes duplicate values from an array when an iteratee is applied", function () {
       const newArr = fi.uniq([1, 2, 2, 3, 4, 6, 9], false, (val) => val % 3);
-      console.log(newArr);
+      // console.log(newArr);
       expect(arraysEqual(newArr, [1, 2, 3])).to.equal(true);
     });
   });
